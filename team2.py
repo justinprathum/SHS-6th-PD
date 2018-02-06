8da94286efa88b7cf58f617734dd3bd189bda92e
Mr. Mainhardt <rmainhar@shakopee.k12.mn.us>
2018-02-06 13:31:22 -0600

    helped Jacob get a control on life

--- team2.py
+++ team2.py
@@ -28,7 +28,7 @@ def move(my_history, their_history, my_score, their_score):
     
     if len(my_history)==0:
         return 'c'
-    elif 'b' in their_history[-3]:
+    elif 'b' in their_history[-3:]:
         return 'b'
     else:
         return 'c'
